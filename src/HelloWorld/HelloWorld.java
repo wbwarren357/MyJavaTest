e95584c973a08c702b0b8bb42b715ec87d2415c4
Will Warren <wbwarren@alum.mit.edu>
2015-12-03 16:54:35 -0500

    Fifth message.

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ package HelloWorld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is a test message.");
-		System.out.println("NEWER:  The new old new doodah.");
+		System.out.println("NEWER:  The new old new doodah.");   
 	}
 
 }
