22b4888c78e7873201a442fb7017533f54af78a1
Will Warren <wbwarren@alum.mit.edu>
2015-12-03 16:48:58 -0500

    Forth commit.

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package HelloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("This is a test message.");
+		System.out.println("NEWER:  The new old new doodah.");
+	}
+
+}
--- src/HelloWorld/HelloWorld2.java
+++ src/HelloWorld/HelloWorld2.java
@@ -1,7 +1,7 @@
 package HelloWorld;
 
 public class HelloWorld2 {
-	public static void main(String[] args) {
+	public static void main2(String[] args) {
 		System.out.println("This is a test message.");
 		System.out.println("The new old new doodah.");
 	}
